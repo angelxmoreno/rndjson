138e0607a9c409eaca63532e750da9501ae4eb30
Angel S. Moreno <angelxmoreno@gmail.com>
2016-10-22 13:53:07 -0400

    Created default module app

---
+++ apps/frontend/controllers/ControllerBase.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Rndjson\Frontend\Controllers;
+
+use Phalcon\Mvc\Controller;
+
+class ControllerBase extends Controller
+{
+
+}
---
+++ apps/frontend/controllers/IndexController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Rndjson\Frontend\Controllers;
+
+class IndexController extends ControllerBase
+{
+
+    public function indexAction()
+    {
+
+    }
+
+}
+
